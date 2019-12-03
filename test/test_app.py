2ae6ad1ba469e2d2442ea6c3f9955b1c23bae451
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-12-03 16:53:51 -0600

    final commit - Unit tests incomplete

---
+++ test/test_app.py
@@ -0,0 +1,36 @@
+import unittest
+from app import database, app
+
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_add_item_success(self):
+        pass
+
+    def test_add_item_fail(self):
+        pass
+
+    def test_update_item_success(self):
+        pass
+
+    def test_update_item_fail(self):
+        pass
+
+    def test_remove_item_success(self):
+        pass
+
+    def test_remove_item_fail(self):
+        pass
+
+    def test_delete_list_success(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
